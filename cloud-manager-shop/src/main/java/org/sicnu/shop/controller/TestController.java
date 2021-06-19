17109619e2e3cc35dff7e26c3456e30c9a21ba3d
WangRey <983604371@qq.com>
2021-06-19 11:37:55 +0800

    add corsFilter

--- cloud-manager-shop/src/main/java/org/sicnu/shop/controller/TestController.java
+++ cloud-manager-shop/src/main/java/org/sicnu/shop/controller/TestController.java
@@ -1,13 +1,16 @@
 package org.sicnu.shop.controller;
 
+import org.sicnu.shop.model.AjaxResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     @GetMapping("/server/{id}")
-    public String server(@PathVariable("id") int id) {
-        return "收到请求Id : " + id + "，结束";
+    @ResponseBody
+    public AjaxResponse server(@PathVariable("id") int id) {
+        return AjaxResponse.success();
     }
 }
---
+++ cloud-manager-zuul/src/main/java/org/sicnu/zuul/config/GateWayCorsConfig.java
@@ -0,0 +1,20 @@
+package org.sicnu.zuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+public class GateWayCorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
